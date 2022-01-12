6f68287982a6a334846bc8efc07e20194f42df62
Franword <jurgafranek@gmail.com>
2022-01-12 14:41:00 +0100

    example update

---
+++ examples/Hexapod_clean/Hexapod_clean.ino
@@ -0,0 +1,98 @@
+#include <Hexapod_lib.h>
+
+Hexapod hexapod;
+Gamepad pad;
+String command;
+int angle_rotz=0, dlugosc_kroku=50;
+int ilosc_odcinkow=3, liczba_krokow=2, delay_micros=1000;
+
+void setup() {
+Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  hexapod.Setup();
+  pad.setup();
+}
+
+void loop() {
+  if(Serial.available()){
+    command=Serial.readStringUntil('\n');
+    command.trim();
+    if(command.equals("init")){
+      Serial.print(command);
+      for(uint8_t leg_num=0;leg_num<6;leg_num++){
+        for(uint8_t servo_num=0;servo_num<3;servo_num++){
+          hexapod.SetServoAngle(90.0,false,leg_num,servo_num);
+        }
+      }
+      hexapod.MoveHexapod();
+    }
+    else if (command.equals("walk")){
+      hexapod.walk(angle_rotz,dlugosc_kroku,ilosc_odcinkow,liczba_krokow, delay_micros);
+    }
+    else if (command.equals("SetOffset")){
+      Serial.println(command);
+      hexapod.SetOffset();
+    }
+    else if(command.equals("current")){
+      hexapod.info(true);
+    }
+    else if(command.equals("offset")){
+      hexapod.info(false);
+    }
+    else if(command.equals("d+")){
+      Serial.println(command);
+      dlugosc_kroku+=10;
+    }
+    else if(command.equals("d-")){
+      Serial.println(command);
+      dlugosc_kroku-=10;
+    }
+    else if(command.equals("a+")){
+      Serial.println(command);
+      angle_rotz+=45;
+    }
+    else if(command.equals("a-")){
+      Serial.println(command);
+      angle_rotz-=45;
+    }
+    else if(command.equals("i+")){
+      Serial.println(command);
+      liczba_krokow+=1;
+    }
+    else if(command.equals("i-")){
+      Serial.println(command);
+      liczba_krokow-=1;
+    }
+    else if(command.equals("n+")){
+      Serial.println(command);
+      ilosc_odcinkow+=1;
+    }
+    else if(command.equals("n-")){
+      Serial.println(command);
+      ilosc_odcinkow-=1;
+    }
+    else if(command.equals("t+")){
+      Serial.println(command);
+      delay_micros+=100;
+    }
+    else if(command.equals("t-")){
+      Serial.println(command);
+      delay_micros-=100;
+    }
+    else{
+      Serial.print("walk(a=");
+      Serial.print(angle_rotz);
+      Serial.print(", d=");
+      Serial.print(dlugosc_kroku);
+      Serial.print(", n=");
+      Serial.print(ilosc_odcinkow);
+      Serial.print(", i=");
+      Serial.print(liczba_krokow);
+      Serial.print(", t=");
+      Serial.print(delay_micros);
+      Serial.println(")");
+    }
+  }
+}
