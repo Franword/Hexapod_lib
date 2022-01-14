6480f5e781266b72ee7c549de243557b4a360724
Franword <jurgafranek@gmail.com>
2022-01-14 12:33:44 +0100

    example update

--- examples/Hexapod_clean/Hexapod_clean.ino
+++ examples/Hexapod_clean/Hexapod_clean.ino
@@ -4,9 +4,11 @@ Hexapod hexapod;
 Gamepad pad;
 String command;
 int angle_rotz=0, dlugosc_kroku=50;
-int ilosc_odcinkow=5, liczba_krokow=2, delay_micros=250;
+//int ilosc_odcinkow=25, liczba_krokow=2, delay_micros=13;
+int ilosc_odcinkow=28, liczba_krokow=2, delay_micros=50, percent_of_moves_without_line=50;
 float pos[3];
 int z=0, y=0, x=0;
+//siema
 void setup() {
 Serial.begin(9600);
   while (!Serial) {
@@ -21,7 +23,7 @@ void loop() {
     command=Serial.readStringUntil('\n');
     command.trim();
     if(command.equals("init")){
-      Serial.print(command);
+      Serial.println(command);
       //float pos[3]={100.0,0.0,-100.0};
       for(uint8_t leg_num=0;leg_num<6;leg_num++){
         for(uint8_t servo_num=0;servo_num<3;servo_num++){
@@ -31,7 +33,7 @@ void loop() {
       hexapod.MoveHexapod();
     }
     else if (command.equals("walk")){
-      hexapod.walk(angle_rotz,dlugosc_kroku,ilosc_odcinkow,liczba_krokow, delay_micros);
+      hexapod.walk(angle_rotz,dlugosc_kroku,ilosc_odcinkow,liczba_krokow, delay_micros,percent_of_moves_without_line);
     }
     else if (command.equals("SetOffset")){
       Serial.println(command);
@@ -42,7 +44,7 @@ void loop() {
       pos[0]=float(x);
       pos[1]=float(y);
       pos[2]=float(z);
-      for(uint8_t i=0;i<10;i++){
+      for(uint8_t i=0;i<10;i+=2){
           for(uint8_t leg_num=0;leg_num<6;leg_num++){
           hexapod.SetLegPos(pos,leg_num,true);  
       }
@@ -87,13 +89,21 @@ void loop() {
       Serial.println(command);
       ilosc_odcinkow-=1;
     }
+    else if(command.equals("p+")){
+      Serial.println(command);
+      percent_of_moves_without_line+=5;
+    }
+    else if(command.equals("p-")){
+      Serial.println(command);
+      percent_of_moves_without_line-=5;
+    }
     else if(command.equals("t+")){
       Serial.println(command);
-      delay_micros+=50;
+      delay_micros+=1;
     }
     else if(command.equals("t-")){
       Serial.println(command);
-      delay_micros-=50;
+      delay_micros-=1;
     }
     else if (command.equals("z")){
       if(z>=1){
@@ -136,6 +146,8 @@ void loop() {
       Serial.print(liczba_krokow);
       Serial.print(", t=");
       Serial.print(delay_micros);
+      Serial.print(", p=");
+      Serial.print(percent_of_moves_without_line);
       Serial.println(")");
     }
   }
